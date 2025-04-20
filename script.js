6a2cbb96552a81203b0611249703404334b2930a
Zaid <142177842+zaidbinshams@users.noreply.github.com>
2025-04-20 22:45:13 +0530

    resized container, added design

--- script.js
+++ script.js
@@ -5,17 +5,14 @@ function makeTheGrid() {
         const subDiv = document.createElement("div");
         subDiv.setAttribute("class", "row");
         subDiv.setAttribute("style",
-            "flex: 1; display: flex; height: 6.25%; width: 100%; background-color: green; outline: 1px solid white;")
+            "display: flex; height: 6.25%; width: 100%; background-color: rgb(200,200,200); outline: 1px solid white;")
         for (let j = 0; j < 16; j++) {
             const square = document.createElement("div");
             square.setAttribute("class", "unit");
             square.setAttribute("style",
-                "flex: 1; height: 100%; width: 6.25%; aspect-ratio: 1/1; outline: 1px solid purple");
-            square.textContent = `${i+1},${j+1}`;
+                "flex: 1; height: 100%; width: 6.25%; aspect-ratio: 1/1; outline: 1px solid white");
             subDiv.appendChild(square);
         }
-        // console.log(subDiv);
-        // console.log(container);
         container.appendChild(subDiv);
     }
 }
